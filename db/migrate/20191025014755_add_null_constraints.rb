04fa1a85da6302ec3a63904b5519021f04ebdae4
JupiterDev <ilyaklyus@gmail.com>
2019-10-26 08:54:09 +0300

    update part3

--- db/migrate/20191025014755_add_null_constraints.rb
+++ db/migrate/20191025014755_add_null_constraints.rb
@@ -1,11 +1,13 @@
 class AddNullConstraints < ActiveRecord::Migration[6.0]
   def change
+    change_column_default(:tests, :level, from: nil, to: 1)
+    change_column_default(:answers, :correct, from: nil, to: false)
+    
     change_column_null(:tests, :title, false)
     change_column_null(:questions, :body, false)
     change_column_null(:users, :username, false)
     change_column_null(:answers, :body, false)
     change_column_null(:categories, :title, false)
-    change_column(:tests, :level, :integer, default: 1)
-    change_column(:answers, :correct, :boolean, default: false)
+    change_column_null(:answers, :correct, false)
   end
 end
