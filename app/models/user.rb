801c8fbe8c0ae426e4be51a8d783dbc978fa9a24
JupiterDev <ilyaklyus@gmail.com>
2019-11-11 07:22:03 +0300

    part9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
-  has_many :selected_tests, dependent: :destroy
-  has_many :tests, through: :selected_tests
-
-  has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :destroy
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
+  has_many :authored_tests, class_name: "Test", foreign_key: "author_id", dependent: :destroy
 
   validates :email, presence: true
 
   def active_tests(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
