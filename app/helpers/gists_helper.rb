a5d512eb404f671e6517c9bed1cfd1c7692a6661
JupiterDev <ilyaklyus@gmail.com>
2019-11-19 01:17:22 +0300

    fixed part12

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,9 +1,9 @@
 module GistsHelper
 	def question_part(gist)
-		gist.question.body[0..24]
+		truncate(gist.question.body, length: 25)
 	end
 
 	def gist_hash(url)
-		url[24..-1]
+		url.split('/').last
 	end
 end
